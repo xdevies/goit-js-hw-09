1ba12cd81678421b5d886cf62187e85afad3cd2d
xdevies <117025533+xdevies@users.noreply.github.com>
2023-04-09 11:27:36 +0000

    Deploying to gh-pages from @ xdevies/goit-js-hw-09@03b0e88edbba782fff56df86a6eb94a8420e8ae3 🚀

---
+++ 01-color-switcher.76d88007.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.76d88007.js.map
